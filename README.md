61fdd89a2943b90b400fa2a8cf7be3dfbdbc54d9
jonaed1230 <jonaed.com.bd@gmail.com>
2019-03-14 22:16:35 +0600

    Make some Changes

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Telegram English To Bangla Translator Bot
 ### How to use:
 - From Telegram go to BotFather Chennel and create a bot they will provide you a token and also a bot chennel url
-- Create a .env File and place that token like this 
-```
-BOT_TOKEN=somethingsomethingxyz
-```
+- Create a .env File and place that token like example.env file 
+- Make your bot as channel admin of both of your channel. 
 - open your terminal go to your project folder and run 
 ```console
 $ npm i
@@ -17,4 +15,4 @@ $ yarn
 ```console
 $ node index.js
 ```
-- That's all now open that bot Channel url and enjoy translating :)
+- It will hear every message from your channel and post translated language to your provided channel.
--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 require('dotenv').config({ path: '.env' })
 const Telegraf = require('telegraf')
 const translate = require('@k3rn31p4nic/google-translate-api')
-const translateTo = 'bn'
-// listen for every text and which language to return
+
+// note: If we put language in .env file it isn't working
+
+const language = 'bn'
+const botToken = process.env.BOT_TOKEN
+const channelId = process.env.CHANNEL_ID
+
+// take text and provided language
 const translator = async (text, lang) => {
   try {
-    // translate text to provided language and if any error happen console.log that
+    // translate text to provided language
     const translated = await translate(text, { to: lang })
     return translated.text
   } catch (error) {
     console.log(error)
   }
 }
-// make a telegraf bot
-const bot = new Telegraf(process.env.BOT_TOKEN)
-// pass the text and provided language to translator function and reply translated text
+// make a telegraf instance
+const bot = new Telegraf(botToken);
+// listen for every chat message and translate them
 bot.use(async ctx => {
   const { text } = ctx.channelPost
-  const translated = await translator(text, translateTo)
+  const translated = await translator(text, language)
 
-  // ctx.reply(translated);
-  // test
+// send Translated message to provided channel
   try {
-    await ctx.telegram.sendMessage(-1001169469570, translated) // remove this hardcoded chat-id and place your own chat-id
-    // just Copy numbers after 'c' and before '_' then add extra -100 before it.
+    await ctx.telegram.sendMessage(Number(`-100${channelId}`), translated)
   } catch (err) {
     console.log(err)
   }
