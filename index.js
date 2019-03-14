2f02915de0c1cbb1af1d4d67895d743bb80badb2
Ashik <ashiquejava@gmail.com>
2019-03-14 19:38:27 +0600

    some refactoring has been done

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-require('dotenv').config({ path: '.env' })
-const Telegraf = require('telegraf')
-const translate = require('@k3rn31p4nic/google-translate-api')
-const translateTo = 'bn'
+require('dotenv').config();
+const Telegraf = require('telegraf');
+const translate = require('@k3rn31p4nic/google-translate-api');
+
+const translateTo = 'bn';
+const channelId = Number(`-100${process.env.CHANNEL_ID}`);
 // listen for every text and which language to return
 const translator = async (text, lang) => {
   try {
-    // translate text to provided language and if any error happen console.log that
-    const translated = await translate(text, { to: lang })
-    return translated.text
+    // translate text to provided language
+    const translated = await translate(text, { to: lang });
+    return translated.text;
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    return null;
   }
-}
+};
 // make a telegraf bot
-const bot = new Telegraf(process.env.BOT_TOKEN)
-// pass the text and provided language to translator function and reply translated text
-bot.use(async ctx => {
-  const { text } = ctx.channelPost
-  const translated = await translator(text, translateTo)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-  // ctx.reply(translated);
-  // test
+bot.use(async (ctx) => {
+  const { text } = ctx.channelPost; // grab message from channel1
+  const translated = await translator(text, translateTo);
   try {
-    await ctx.telegram.sendMessage(-1001169469570, translated) // remove this hardcoded chat-id and place your own chat-id
-    // just Copy numbers after 'c' and before '_' then add extra -100 before it.
+    // channel has to select bot as admin in order to recieve message.
+    // sending translated message on target channel
+    await ctx.telegram.sendMessage(channelId, translated);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-})
+});
 
-bot.launch()
+bot.launch();
